d04be6aa7859ba5ddcc09a057fec0cf61e066396
Eyram K. Apetcho <eyram.apetcho@gmail.com>
2025-02-14 14:19:43 +0100

    Implement Lynx::Object::operator-=(const Object&)

--- src/lynx-object.cpp
+++ src/lynx-object.cpp
@@ -1954,8 +1954,34 @@ Object& Object::operator+=(const Object& lhs){
     return *this;
 }
 
+/**
+ * @brief Implement "-=" operatin on object.
+ * 
+ * The '-=' operator is applicable on the following builtin types:
+ * 
+ * (1) Integer: (x -= y) -> Integer
+ * (2) Float: (x -= y) -> Float
+ *      - Float -= Integer -> Float
+ *      - Integer -= Float -> Float
+ * (3) Complex: (z1 -= z2) -> Complex
+ *      - Complex -= Number -> Complex
+ * (4) HashSet: (lhs -= rhs) -> HashSet
+ * 
+ * @param lhs 
+ * @param rhs 
+ * @return Object 
+ */
+Object& Object::operator-=(const Object& lhs){
+    Args args{};
+    args.push_back(std::make_shared<Object>(*this));
+    args.push_back(std::make_shared<Object>(lhs));
+    Self self = Object().__sub__(args);
+    this->m_value = std::move(self->m_value);
+
+    return *this;
+}
+
 /*
-Object& Object::operator-=(const Object& lhs){}
 Object& Object::operator/=(const Object& lhs){}
 Object& Object::operator%=(const Object& lhs){}
 Object& Object::operator*=(const Object& lhs){}
